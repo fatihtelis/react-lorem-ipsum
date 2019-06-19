136eda8fa1ce60983a0a4c64b04afb75a671a3de
Fatih Telis <fatih.telis@fabrikamedya.com.tr>
2019-06-19 17:30:44 +0300

    Favicon addition and assets folder creation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, 'examples/src/index.html'),
   filename: './index.html',
 });
+
+const copyWebpackPlugin = new CopyWebpackPlugin([{ from: 'examples/src/assets', to: 'assets' }]);
 
 module.exports = {
   entry: path.join(__dirname, 'examples/src/index.js'),
@@ -25,7 +28,7 @@ module.exports = {
       },
     ],
   },
-  plugins: [htmlWebpackPlugin],
+  plugins: [htmlWebpackPlugin, copyWebpackPlugin],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
